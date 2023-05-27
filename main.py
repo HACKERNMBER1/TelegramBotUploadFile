8c73f44828de582c477b36dfd94cc19d34f08ba3
HACKERNMBER1 <94354827+HACKERNMBER1@users.noreply.github.com>
2023-05-27 13:06:25 +0300

    Update main.

--- main.py
+++ main.py
@@ -1,64 +1,33 @@
 import os
-import requests
+import time
 from pyrogram import Client, filters
 
-TOKEN = "6269468548:AAG8upYBql2WDRYIdAGxnawJMycmj0WM7sE"
-API_ID = "10311512"
-API_HASH = "49589a9b575a64954e9f59062c2a3e76"
+API_ID = 10311512
+API_HASH = '49589a9b575a64954e9f59062c2a3e76'
+BOT_TOKEN = '6269468548:AAG8upYBql2WDRYIdAGxnawJMycmj0WM7sE'
 
-app = Client("my_bot", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH)
-
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 welcome_message = "مرحبًا! أنا بوت تليجرام لرفع الملفات. قم بإرسال ملف لي وسأقوم برفعه لك."
 
+@app.on_message(filters.command('start'))
+async def send_welcome(_, message):
+    await message.reply_text(f"*{welcome_message}*", parse_mode="Markdown")
 
-@app.on_message(filters.command("start"))
-def send_welcome(client, message):
-    client.send_message(
-        message.chat.id,
-        f"*{welcome_message}*",
-        parse_mode="Markdown"
-    )
-
-
-def upload_file(file_url, file_name):
-    response = requests.get(file_url)
-    file_data = response.content
-
-    with open(file_name, "wb") as file:
-        file.write(file_data)
-
-    response = requests.post("https://api.anonfiles.com/upload", files={'file': open(file_name, "rb")})
-
-    if response.status_code == 200:
-        data = response.json()
-        if data['status']:
-            return data['data']['file']['url']['short']
-
-    return None
-
+def upload_file(file_path):
+    with open(file_path, 'rb') as file:
+        response = app.send_document("YOUR_TELEGRAM_CHANNEL_ID", file)
+        return response.link
 
 @app.on_message(filters.document)
-def handle_document(client, message):
-    file_info = message.document
-    file_url = client.get_download_url(file_info)
+async def handle_document(_, message):
     file_name = message.document.file_name
-    upload_link = upload_file(file_url, file_name)
-
+    file_path = await app.download_media(message)
+    upload_link = upload_file(file_path)
     if upload_link:
-        client.send_message(
-            message.chat.id,
-            f"*تم رفع الملف بنجاح!\n\nرابط التحميل: *{upload_link}\n\nلشراء أو تفعيل بوت مماثل لهذا، تواصل معي @VIP3GL",
-            disable_web_page_preview=True,
-            parse_mode="Markdown"
-        )
+        await message.reply_text(f"*تم رفع الملف بنجاح!\n\nرابط التحميل: *{upload_link}\n\nلشراء أو تفعيل بوت مماثل لهذا، تواصل معي @VIP3GL", disable_web_page_preview=True, parse_mode="Markdown")
     else:
-        client.send_message(
-            message.chat.id,
-            "*حدث خطأ أثناء رفع الملف.*",
-            parse_mode="Markdown"
-        )
-
+        await message.reply_text("*حدث خطأ أثناء رفع الملف.*", parse_mode="Markdown")
 
 if __name__ == "__main__":
     app.run()
